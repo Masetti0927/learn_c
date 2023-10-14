57e6e0c3f4aa95d20a22c82ef72b9796620632f3
Masetti0927 <2576632761@qq.com>
2023-10-14 22:19:14 +0800

    10月14日晚更改

--- test.c
+++ test.c
@@ -1,12 +1,15 @@
 #include<stdio.h>
-void max(int x, int y)
+//今天是周六，又到了可以休息的日子了，7天后又会是一个周六。
+//请你帮忙计算下，n（n为整数，可以是正也可以为负）天后会是星期几呢？
+int main()
 {
-	if (x > y)
-		printf("%d", x);
+	int n;
+	char* a[8] = {"六","日","一","二","三","四","五","六"};
+	scanf_s("%d", &n);
+	n = n % 7;
+	if (n >= 0)
+		printf("星期%s", a[n]);
 	else
-		printf("%d", y);
-}
-void main()
-{
-	max(3, 5);
+		printf("星期%s", a[-n-1]);
+	return 0;
 }
